186e4af57e7edab1038ad0c124dbeb672bc95f72
Yosiel <ycuello85@gmail.com>
2019-09-24 13:35:14 -0700

    remove file with upload aliyun

--- cron-backup-db/alibaba_upload.py
+++ cron-backup-db/alibaba_upload.py
@@ -11,19 +11,13 @@ date_actual = datetime.now().date()
 # First, initialize the AccessKeyId, AccessKeySecret, Endpoint and other information.
 # Obtained by environment variables, or by such asâ€œ<your_AccessKeyId>â€Replace with a real AccessKeyId and so on.
 #
-access_key_id = os.getenv('OSS_TEST_ACCESS_KEY_ID', 'LTAIGmg7WXMnXaOi')
-access_key_secret = os.getenv('OSS_TEST_ACCESS_KEY_SECRET', '6vFzQR3ZVb0Xf3Rg6uZNZZwUv4y3JV')
-bucket_name = os.getenv('OSS_TEST_BUCKET', 'dev-trash-helpers')
-endpoint = os.getenv('OSS_TEST_ENDPOINT', 'https://oss-us-west-1.aliyuncs.com')
-
-
-# Confirm that the above parameters are filled in correctly.
-for param in (access_key_id, access_key_secret, bucket_name, endpoint):
-    assert '<' not in param, 'Please set the parameters:' + param
-
+#access_key_id = os.getenv('OSS_TEST_ACCESS_KEY_ID', 'LTAIGmg7WXMnXaOi')
+#access_key_secret = os.getenv('OSS_TEST_ACCESS_KEY_SECRET', '6vFzQR3ZVb0Xf3Rg6uZNZZwUv4y3JV')
+#bucket_name = os.getenv('OSS_TEST_BUCKET', 'staging-bucket')
+#endpoint = os.getenv('OSS_TEST_ENDPOINT', 'https://oss-us-west-1.aliyuncs.com')
 
 # Create a bucket object, all Object related interfaces can be done through the Bucket object
-bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, bucket_name)
+bucket = oss2.Bucket(oss2.Auth('OSS_TEST_ACCESS_KEY_ID', 'OSS_TEST_ACCESS_KEY_SECRET'), 'OSS_TEST_ENDPOINT', 'OSS_TEST_BUCKET')
 
 # Generate a local file for testing. The file content is of type bytes.
 filepath = "/tmp/backups/donex_db_backup_" + str(date_actual)
@@ -32,12 +26,7 @@ filename = filepath + '.tar.gz'
 #filename = './test.png'
 
 #bucket.put_object_from_file('test-upload.png', "./cron-backup-db/test.png")
-bucket.put_object_from_file('donex_db_backup_'+ str(date_actual) +'.tar.gz', filepath)
-
-# You can also directly call the slice upload interface.
-# First, you can use the help function to set the slice size, and let us expect the slice size to be 128KB.
-# total_size = os.path.getsize(filename)
-# part_size = oss2.determine_part_size(total_size, preferred_size=128 * 1024)
+bucket.put_object_from_file('donex_db_backup_'+ str(date_actual) +'.tar.gz', filename)
 
 # Remove file name path
-#os.remove(filename)
+os.remove(filename)
