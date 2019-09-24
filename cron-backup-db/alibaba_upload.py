8b731102d8c94b13e9dc87ae6898cd12bec6a99a
Yosiel <ycuello85@gmail.com>
2019-09-24 11:47:22 -0700

    generate database backup and configuring alibaba

--- cron-backup-db/alibaba_upload.py
+++ cron-backup-db/alibaba_upload.py
@@ -6,9 +6,9 @@ import string
 import oss2
 from datetime import datetime
 
-date_actual = datetime.now()
+date_actual = datetime.now().date()
 # First, initialize the AccessKeyId, AccessKeySecret, Endpoint and other information.
-# Obtained by environment variables, or by such as“<your_AccessKeyId>”Replace with a real AccessKeyId and so on.
+# Obtained by environment variables, or by such asâ€œ<your_AccessKeyId>â€Replace with a real AccessKeyId and so on.
 #
 access_key_id = os.getenv('OSS_TEST_ACCESS_KEY_ID', 'LTAIGmg7WXMnXaOi')
 access_key_secret = os.getenv('OSS_TEST_ACCESS_KEY_SECRET', '6vFzQR3ZVb0Xf3Rg6uZNZZwUv4y3JV')
@@ -25,13 +25,13 @@ for param in (access_key_id, access_key_secret, bucket_name, endpoint):
 bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, bucket_name)
 
 # Generate a local file for testing. The file content is of type bytes. 
-filepath = "/opt/backups/donex_db_backup/donex_db_backup_" + date_actual.date()
+filepath = "/tmp/backups/donex_db_backup_" + str(date_actual)
 
 filename = filepath + '.tar.gz'
 
 # Breakpoint resume: Because the file is small (less than oss2.defaults.multipart_threshold),
 # So actually use oss2.Bucket.put_object
-oss2.resumable_upload(bucket, 'remote-normal.txt', filename)
+oss2.resumable_upload(bucket, filename)
 
 # You can also directly call the slice upload interface.
 # First, you can use the help function to set the slice size, and let us expect the slice size to be 128KB.
@@ -39,4 +39,4 @@ total_size = os.path.getsize(filename)
 part_size = oss2.determine_part_size(total_size, preferred_size=128 * 1024)
 
 # Remove file name path
-os.remove(filename)
+#os.remove(filename)
--- cron-backup-db/backup-donex-shell.sh
+++ cron-backup-db/backup-donex-shell.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # vars
-BACKUPS_PATH="/opt/backups";
-CURRENT_DATE_TIME="`date +%d-%m-%Y`";
+BACKUPS_PATH="/tmp/backups";
+CURRENT_DATE_TIME="`date +%Y-%m-%d`";
 FILE_NAME="$BACKUPS_PATH/donex_db_backup"
-DATABASE_NAME="donex_db_backup_${CURRENT_DATE_TIME}.sql";
 
-export PGUSER=postgres
-export PGPASSWORD=postgres;
+export PGUSER=backup
+export PGPASSWORD=mustbackupdb....!!!!
 
 # generate and accessing backup_path
 mkdir -p $BACKUPS_PATH;
@@ -16,21 +15,29 @@ cd $BACKUPS_PATH;
 # We set the default permissions
 umask 177
 
-# dump
-echo "processing the database copy..."
-# pg_dump -h localhost --port 5432 -f ${DATABASE_NAME} $DB_NAME;
-pg_dumpall -h localhost --port 5432 -f ${DATABASE_NAME};
-echo "backup finished."
+# Declare Database array variable
+declare -a db=("donex_accounts_staging" "donex_catalog_staging" "donex_logistics_staging" "donex_payments_staging" "donex_files_staging" "donex_os_staging" "donex_customer_service_staging" "donex_wallet_staging" "donex_social_staging" "donex_notifications_staging" "donex_prod")
+
+echo "******************"
+for db in "${db[@]}"
+do
+  DATABASE_NAME="${db}"
+  DATABASE_NAME_BACKUP="${db}_backup_${CURRENT_DATE_TIME}.dump"
+  echo "${DATABASE_NAME} bachup finished."
+  pg_dump -U backup -h localhost --port 5432 -x -O -f $FILE_NAME/${DATABASE_NAME_BACKUP} ${DATABASE_NAME};
+ 
+done
+echo "******************"
+
+tar -cvzpf $BACKUPS_PATH/donex_db_backup_$CURRENT_DATE_TIME.tar.gz $FILE_NAME
 
 # get size of dump for the future
-size="`wc -c $BACKUPS_PATH/$DATABASE_NAME`";
+size="`wc -c $BACKUPS_PATH/donex_db_backup_$CURRENT_DATE_TIME.tar.gz`";
 echo $size;
 
-tar -cvzpf $FILE_NAME/donex_db_backup_$CURRENT_DATE_TIME.tar.gz $BACKUPS_PATH/$DATABASE_NAME
+python /home/admin/scripts/backup/alibaba_upload.py
 
-python $HOME/cron/alibaba_upload.py
-
-#rm -rf $FILE_NAME
+rm -rf $FILE_NAME
 #rm $DATABASE_NAME
 
 unset PGUSER
