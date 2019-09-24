7d1618c43595759f8353b9e06b268fec68d04c3c
Jesús Luongo Lizana <jesus.luongo.lizana@gmail.com>
2019-09-24 13:32:05 -0300

    progress upload aliyun

--- cron-backup-db/alibaba_upload.py
+++ cron-backup-db/alibaba_upload.py
@@ -5,6 +5,7 @@ import random
 import string
 import oss2
 from datetime import datetime
+from pathlib import Path
 
 date_actual = datetime.now().date()
 # First, initialize the AccessKeyId, AccessKeySecret, Endpoint and other information.
@@ -13,7 +14,7 @@ date_actual = datetime.now().date()
 access_key_id = os.getenv('OSS_TEST_ACCESS_KEY_ID', 'LTAIGmg7WXMnXaOi')
 access_key_secret = os.getenv('OSS_TEST_ACCESS_KEY_SECRET', '6vFzQR3ZVb0Xf3Rg6uZNZZwUv4y3JV')
 bucket_name = os.getenv('OSS_TEST_BUCKET', 'dev-trash-helpers')
-endpoint = os.getenv('OSS_TEST_ENDPOINT', 'oss-us-west-1')
+endpoint = os.getenv('OSS_TEST_ENDPOINT', 'https://oss-us-west-1.aliyuncs.com')
 
 
 # Confirm that the above parameters are filled in correctly.
@@ -24,19 +25,18 @@ for param in (access_key_id, access_key_secret, bucket_name, endpoint):
 # Create a bucket object, all Object related interfaces can be done through the Bucket object
 bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, bucket_name)
 
-# Generate a local file for testing. The file content is of type bytes. 
+# Generate a local file for testing. The file content is of type bytes.
 filepath = "/tmp/backups/donex_db_backup_" + str(date_actual)
 
-filename = filepath + '.tar.gz'
+# filename = filepath + '.tar.gz'
+filename = './test.png'
 
-# Breakpoint resume: Because the file is small (less than oss2.defaults.multipart_threshold),
-# So actually use oss2.Bucket.put_object
-oss2.resumable_upload(bucket, filename)
+bucket.put_object_from_file('test-upload.png', "./cron-backup-db/test.png")
 
 # You can also directly call the slice upload interface.
 # First, you can use the help function to set the slice size, and let us expect the slice size to be 128KB.
-total_size = os.path.getsize(filename)
-part_size = oss2.determine_part_size(total_size, preferred_size=128 * 1024)
+# total_size = os.path.getsize(filename)
+# part_size = oss2.determine_part_size(total_size, preferred_size=128 * 1024)
 
 # Remove file name path
 #os.remove(filename)
