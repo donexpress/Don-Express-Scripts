cb8c538e6f0453c50f642cb760fa7904c600aab0
Yosiel Medero Cuello <ycuello85@gmail.com>
2020-09-19 11:30:27 -0400

    wip

--- publish/backend/publish_staging_app.sh
+++ publish/backend/publish_staging_app.sh
@@ -13,14 +13,16 @@ if [[ ($2 != "customer_service") && ($2 != "cs_channel") ]];
 then
     docker pull registry.donexpress.com/$environment-$2:staging;
 else
-    if [$2 == "customer_service"];
+    if [$2 == 'customer_service']
     then
-        app='customer-service';
+        app="customer-service"
+        echo docker pull registry.donexpress.com/$environment-$app:staging
         docker pull registry.donexpress.com/$environment-$app:staging;
     fi
-    if [$2 == "cs_channel"];
+    if [$2 == 'cs_channel']
     then
-        app='cs-channel';
+        app="cs-channel"
+        echo docker pull registry.donexpress.com/$environment-$app:staging
         docker pull registry.donexpress.com/$environment-$app:staging;
     fi
 fi
