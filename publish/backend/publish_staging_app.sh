fc1a774b015cb1cfc9462b2f57a271dff4dddef6
Yosiel Medero Cuello <ycuello85@gmail.com>
2020-09-23 15:17:22 -0400

    up deploy

--- publish/backend/publish_staging_app.sh
+++ publish/backend/publish_staging_app.sh
@@ -18,16 +18,14 @@ if [[ ($2 != 'customer_service') && ($2 != 'cs_channel') ]];
 then
     docker pull registry.donexpress.com/$environment-$2:staging;
 else
-    if [$2 == 'customer_service']
+    if [ $2 == 'customer_service' ]
     then
         app="customer-service"
-        echo docker pull registry.donexpress.com/$environment-$app:staging
         docker pull registry.donexpress.com/$environment-$app:staging;
     fi
-    if [$2 == 'cs_channel']
+    if [ $2 == 'cs_channel' ]
     then
         app="cs-channel"
-        echo docker pull registry.donexpress.com/$environment-$app:staging
         docker pull registry.donexpress.com/$environment-$app:staging;
     fi
 fi
@@ -35,9 +33,9 @@ fi
 cd ~/donex_$2/
 
 #git stash;
-if [$3 == 'master']
+if [ $3 == 'master' ]
 then
-    exit 1;
+    exit 0;
 fi
 git pull origin $3;
 
